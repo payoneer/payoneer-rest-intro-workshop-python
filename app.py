8fea31d534cf4b9aff1029999e4282123b84373f
Arnold Simha <arnoldsi@payoneer.com>
2022-11-10 16:20:01 +0200

    add decorators

--- app.py
+++ app.py
@@ -1,7 +1,9 @@
 from flask import Flask, Response, request
 
-from .models.item import Item
+from .utils.json_decorator import as_json
+
 from .data.seed import items
+from .models.item import Item
 
 app = Flask("app",
             static_url_path='', 
@@ -14,17 +16,19 @@ def root():
 
 
 @app.route("/items")
+@as_json
 def get_items():
-    return Response(str(items), content_type="application/json")
+    return items
 
 
 @app.route('/items', methods=["POST"])
+@as_json
 def add_item():
     body = request.get_json()
     name = body["name"]
     item = Item(name);
     items.append(item)
-    return Response(str(item), content_type="application/json")
+    return item
 
 @app.route('/items/<id>', methods=['PATCH'])
 def update_item(id):
